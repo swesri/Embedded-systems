76c028b96e563e6f8a97170a4d60acb18ec00d6c
swesri <swesri91@gmail.com>
2017-05-09 23:49:44 -0600

    4th program upload

--- 4reverse_ll.c
+++ 4reverse_ll.c
@@ -1,5 +1,28 @@
+/*4reverse_ll.c
+Author: Swetha Varadarajan
+Date created: May 9,2017
+
+Reversal of linked list
+
+*/
+
+
 #include<stdio.h>
+#include<stdlib.h>
+#include"linked_list.h"
 
-void main() {
 
+void main(int argc,char** argv) 
+{
+	struct node* head = NULL;
+	append(&head, 6);
+	push(&head, 7);
+	push(&head, 1);
+	append(&head, 4);
+	insertAfter(head->next, 8);
+	printf("Created Linked list is: \n");
+	printList(head);
+	printf("Reversed Linked list is: \n");
+	reverse(&head);
+	printList(head);
 }
