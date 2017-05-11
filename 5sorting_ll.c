8081125a23542e944d29eb4602d2c0edb4192fc4
swesri <swesri91@gmail.com>
2017-05-11 01:17:36 -0600

    5th program upload

--- 5sorting_ll.c
+++ 5sorting_ll.c
@@ -1,7 +1,32 @@
+/*5sorting_ll.c
+Author: Swetha Varadarajan
+Date created: May 9,2017
+
+Reversal of linked list
+
+*/
+
+
 #include<stdio.h>
+#include<stdlib.h>
+#include"linked_list.h"
 
-void main()
+
+void main(int argc,char** argv) 
 {
+	struct node *a = NULL;
+	push(&a, 5);
+	push(&a, 20);
+	push(&a, 4);
+	push(&a, 3);
+	push(&a, 30);
 
+	printf("Linked List before sorting \n");
+	printList(a);
+
+	quickSort(&a);
+
+	printf("Linked List after sorting \n");
+	printList(a);
 
 }
