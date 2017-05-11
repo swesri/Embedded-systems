f076d2d0b7ecea489f1168442647e723b5594712
swesri <swesri91@gmail.com>
2017-05-11 01:27:29 -0600

    uploading 6th program

--- 6cirtest_ll.c
+++ 6cirtest_ll.c
@@ -1,5 +1,27 @@
+/*6cirtest_ll.c
+Author: Swetha Varadarajan
+Date created: May 11,2017
+
+Program to test if the linked list is circular
+
+*/
+
 #include<stdio.h>
+#include<stdlib.h>
+#include"linked_list.h"
 
-void main(){
+void main(int argc,char** argv) 
+{
+	struct node *a = NULL;
+	push(&a, 5);
+	push(&a, 20);
+	push(&a, 4);
+	push(&a, 3);
+	push(&a, 30);
 
+	isCircular(a)? printf("The list is circular\n") : printf("The list is not circular\n") ;
+ 
+	a->next->next->next->next = a;
+ 
+    	isCircular(a)? printf("The list is circular\n") : printf("The list is not circular\n") ;
 }
