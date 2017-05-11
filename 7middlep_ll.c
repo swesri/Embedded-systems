79afde078a454dd7d69217f6a1df820946024a90
swesri <swesri91@gmail.com>
2017-05-11 01:31:37 -0600

    uploading 7th program

--- 7middlep_ll.c
+++ 7middlep_ll.c
@@ -1,5 +1,25 @@
+/*7middlep_ll.c
+Author: Swetha Varadarajan
+Date created: May 11,2017
+
+Program to print middle of linked list
+
+*/
+
 #include<stdio.h>
+#include<stdlib.h>
+#include"linked_list.h"
 
-void main(){
+void main(int argc,char** argv) 
+{
+	struct node* head = NULL;
+	int i;
 
+	for (i=5; i>0; i--)
+	{
+		push(&head, i);
+		printList(head);
+		printMiddle(head);
+	}
+ 
 }
