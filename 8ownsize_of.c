db869acb25dae331422eb38659c19fc21d9f7653
swesri <swesri91@gmail.com>
2017-05-11 12:20:25 -0600

    8th program upload

--- 8ownsize_of.c
+++ 8ownsize_of.c
@@ -1,7 +1,21 @@
+/*8ownsize_of.c
+Author: Swetha Varadarajan
+Date created: May 11,2017
+
+Program to implement the sizeof() function
+
+*/
+
 #include<stdio.h>
+#define mysizeof(p) ((char*)(&p+1) - (char*)(&p))
 
-void main()
+void main(int argc,char** argv)
 {
+	int a;
+	double b;
+	char c;
 
-
+	printf("Size of int is %ld bytes\n",mysizeof(a));
+	printf("Size of double is %ld bytes\n",mysizeof(b));
+	printf("Size of char is %ld bytes\n",mysizeof(c));
 }
